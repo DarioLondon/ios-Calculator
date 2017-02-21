3b3931044dfd9801466ad25a6a97e7bf51455fd5
Dario <dario.guida@my.westminster.ac.uk>
2017-02-21 16:50:02 +0000

    Fuck sake

--- Calculator/CalculatorEngine.swift
+++ Calculator/CalculatorEngine.swift
@@ -9,17 +9,11 @@
 import Foundation
 import UIKit
 
-class CalculatorEngine{
+class CalculatorEngine : NSObject {
     
-    var numberStack :[Double]!
-    var operandStack : [String]!
-    let operand :Operand!
+    var numberStack :[Double] = [Double]()
+    let operands :Operand = Operand.init()
     
-    init() {
-        self.numberStack = [Double]()
-        self.operandStack = [String]()
-        self.operand = Operand.init()
-    }
     
     
     
@@ -27,58 +21,144 @@ class CalculatorEngine{
         self.numberStack.append(item)
     }
     
-    func addOperand(_ item:String){
-        self.operandStack.append(item)
-    }
-    
-    func removeLastDigitedNumber(){
-       self.numberStack.removeLast()
-    }
     
     func cleanTheStacks(){
-        self.operandStack.removeAll()
+      
         self.numberStack.removeAll()
     }
     
-    func calculate()-> Double{
+    func binaryCalculation(operand:String)-> Double{
         
-        switch (self.operandStack.removeFirst()){
+        switch (operand){
             
             
-        case operand.plus :
+        case operands.plus :
                 if self.numberStack.count >= 2
             {
                     return self.numberStack.removeLast() + self.numberStack.removeFirst()
             }
-            case operand.minus :
+            case operands.minus :
                 if self.numberStack.count >= 2
             {
                 return self.numberStack.removeFirst() - self.numberStack.removeFirst()
             }
-            case operand.multiply:
+            case operands.multiply:
     
             if self.numberStack.count >= 2
             {
                 return self.numberStack.removeFirst() * self.numberStack.removeFirst()
             }
-            case operand.divide :
+            case operands.divide :
             
             if self.numberStack.count >= 2
             {
                 return self.numberStack.removeFirst() / self.numberStack.removeLast()
             }
-            case operand.squareRoot :
-                
-            if self.numberStack.count == 1
-            {
-                return sqrt(abs(self.numberStack.removeLast()))
-            }
-            
+          
         default:break
             
             
         }
         return 0.0
+    }
+    
+    func UnaryCalculation(unaryOperand:String, _ isRadinant :Bool = false )->Double{
+        
+        switch(unaryOperand){
+        
+        case operands.squareRoot:
+            if self.numberStack.count >= 1 {
+                return sqrt(abs(numberStack.removeLast()))
+            }
+        case operands.powerOfTwo:
+            if self.numberStack.count >= 1 {
+               
+                return pow(self.numberStack.last!,self.numberStack.removeLast())
+            }
+        case operands.sin:
+            if self.numberStack.count >= 1 {
+                
+                    if isRadinant {
+                        return ((self.numberStack.last!.truncatingRemainder(dividingBy: M_PI)) == 0.0)
+                            ? 0.0
+                            :  __sinpi(self.numberStack.removeLast()/180)
+                    }else{
+                    
+                        return ((self.numberStack.last!.truncatingRemainder(dividingBy: 180.0)) == 0.0)
+                            ? 0.0
+                            : sin(self.numberStack.removeLast())
+                    
+                    }
+            }
+            
+        case operands.cosh:
+            if self.numberStack.count >= 1 {
+                if isRadinant {
+                    return ((self.numberStack.last!.truncatingRemainder(dividingBy: M_PI)) == 0.0)
+                        ? 0.0
+                        :  cosh(self.numberStack.removeLast() * M_PI)/180
+                }else{
+                    
+                    return ((self.numberStack.last!.truncatingRemainder(dividingBy: 180.0)) == 0.0)
+                        ? 0.0
+                        : cosh(self.numberStack.removeLast())
+                    
+                }
+
+            }
+            
+        case operands.cosh:
+            if self.numberStack.count >= 1 {
+                if isRadinant {
+                    return ((self.numberStack.last!.truncatingRemainder(dividingBy: M_PI)) == 0.0)
+                        ? 0.0
+                        :  __cospi(self.numberStack.removeLast()/180)
+                }else{
+                    
+                    return ((self.numberStack.last!.truncatingRemainder(dividingBy: 180.0)) == 0.0)
+                        ? 0.0
+                        : cos(self.numberStack.removeLast())
+                    
+                }
+                
+            }
+            
+        case operands.tan:
+            if self.numberStack.count >= 1 {
+                if isRadinant {
+                    return ((self.numberStack.last!.truncatingRemainder(dividingBy: M_PI)) == 0.0)
+                        ? 0.0
+                        :  __tanpi(self.numberStack.removeLast()/180)
+                }else{
+                    
+                    return ((self.numberStack.last!.truncatingRemainder(dividingBy: 180.0)) == 0.0)
+                        ? 0.0
+                        : tan(self.numberStack.removeLast())
+                    
+                }
+                
+            }
+            
+        case operands.logBaseTen :
+            if self.numberStack.count >= 1 {
+                
+            return log10(self.numberStack.removeLast())
+                
+            }
+            
+        case operands.logBaseE :
+            if self.numberStack.count >= 1 {
+                
+                return log(self.numberStack.removeLast())
+                
+            }
+            
+        default:break
+        }
+    
+    
+        return 0.0
+    
     }
     
     
@@ -95,7 +175,7 @@ struct  Operand {
     let squareRoot :String = "²√"
     let plusMinus :String = "±"
     let pi :String = "π"
-    let xTwo :String = "x²"
+    let powerOfTwo :String = "x²"
     let sin :String = "sin"
     let cos :String = "cos"
     let tan :String = "tan"
@@ -103,6 +183,7 @@ struct  Operand {
     let cosh  :String = "cosh"
     let tanh :String = "tanh"
     let logBaseTen :String = "㏒₁₀"
+    let logBaseE :String = "㏒ℯ"
     
 }
 
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelDisplay: UILabel!
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+        var  cal : CalculatorEngine? 
+   
+    
+    var userHasStartedTyping : Bool = false
+    
+    var displayValue : Double {
+        get{
+            return Double(labelDisplay.text!)!
+        }
+        set(newValue){
+            labelDisplay.text! = "\(newValue)"
+            userHasStartedTyping = false
+        }
+        
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+   
     }
 
 
 }
 
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
